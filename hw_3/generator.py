df8d06ff84996c4dee3b33c27ec977814fa3bdd4
Ivan Leskin <leskin.in@phystech.edu>
2017-09-28 19:26:40 +0300

    Homework 3: generator.py made interactive

--- hw_3/generator.py
+++ hw_3/generator.py
@@ -1,9 +1,23 @@
 import random
+from sys import argv
+
+
+if len(argv) > 2:
+    print("Incorrect command line arguments")
+    exit(-1)
+
+try:
+    range_limit = int(argv[1])
+except:
+    print("Incorrect command line arguments")
+    exit(-1)
+
+
 
 random.seed()
 
 array = []
-for i in range(1, 2500001):
+for i in range(1, range_limit + 1):
     array.append(i)
 
 random.shuffle(array)
