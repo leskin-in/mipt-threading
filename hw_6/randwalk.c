cdd351e3d6c109345ef67bca194769174c3afacb
Ivan Leskin <leskin.in@phystech.edu>
2017-11-06 22:19:16 +0000

    Homework 6: Fix: Global variables now have special names

--- hw_6/randwalk.c
+++ hw_6/randwalk.c
@@ -45,9 +45,9 @@ const int RECEIVE_TICK_INTERVAL = 64;
 const int QUEUE_RAND_NORMAL_LENGTH = 512;
 
 
-int mpi_size;
+int Mpi_size;
 
-int mpi_rank;
+int Mpi_rank;
 
 
 /// Global MPI_Datatype for the PARTICLE struct
@@ -55,16 +55,16 @@ MPI_Datatype PARTICLE_mpi_t;
 
 
 /// System V queue ID for the sender thread
-int qid_send;
+int Qid_send;
 
 /// System V queue ID for the receiver thread
-int qid_receive;
+int Qid_receive;
 
 
-pthread_t thread_processor_tid;
+pthread_t Thread_processor_tid;
 
 // When this is locked, thread_processor cannot act
-pthread_mutex_t thread_processor_lock;
+pthread_mutex_t Thread_processor_lock;
 
 
 /// System V message type and IPC tag: normal message
@@ -89,25 +89,25 @@ pthread_mutex_t thread_processor_lock;
 // Global variables for particle processing //
 
 /// Sector side
-long l;
+long Base_l;
 /// Sector number at X axis
-long a;
+long Base_a;
 /// Sector number at Y axis
-long b;
+long Base_b;
 /// Total number of steps a particle should do
-long n;
+long Base_n;
 /// Total number of particles
-long N;
+long Base_N;
 /// Total number of dead particles
 long N_dead;
 
-double p_left;
-double p_right;
-double p_up;
-double p_down;
+double P_left;
+double P_right;
+double P_up;
+double P_down;
 
 /// Sector offset length
-long offset;
+long Offset;
 
 /// Maximum value of 'offset'
 const long OFFSET_MAX = 64;
@@ -115,18 +115,18 @@ const long OFFSET_MAX = 64;
 /// Divisor of 'offset' calculation. 'offset' = 'l' / 'OFFSET_DIVISOR'
 const long OFFSET_DIVISOR = 5;
 
-long field_width;
-long field_height;
+long Field_width;
+long Field_height;
 
 /// Depot for the active particles (currently processed)
-PARTICLE* depot_a;
+PARTICLE* Depot_a;
 /// Total number of the particles currently being processed
-int depot_a_length;
+int Depot_a_length;
 
 /// Depot for the dead particles (which died in current thread's action zone)
-PARTICLE* depot_d;
+PARTICLE* Depot_d;
 /// Total number of the dead particles in current thread's action zone
-int depot_d_length;
+int Depot_d_length;
 
 
 
@@ -185,7 +185,7 @@ void print_help(void) {
  * @return Sector the 'unit' actually belongs to
  */
 long get_sector(const PARTICLE* unit) {
-    return (unit ->y / l) * a + (unit ->x / l);
+    return (unit ->y / Base_l) * Base_a + (unit ->x / Base_l);
 }
 
 
@@ -202,22 +202,22 @@ long get_sector_with_offset(const PARTICLE* unit, long current_sector) {
     
     int field_borders_reached;
     
-    long x_min = l * (current_sector % a);
-    long y_min = l * (current_sector / a);
-    long x_max = x_min + l - 1;
-    long y_max = y_min + l - 1;
-    x_min -= offset;
-    y_min -= offset;
-    x_max += offset;
-    y_max += offset;
+    long x_min = Base_l * (current_sector % Base_a);
+    long y_min = Base_l * (current_sector / Base_a);
+    long x_max = x_min + Base_l - 1;
+    long y_max = y_min + Base_l - 1;
+    x_min -= Offset;
+    y_min -= Offset;
+    x_max += Offset;
+    y_max += Offset;
     
     field_borders_reached = 0;
     if (x_min < 0) {
-        x_min += field_width;
+        x_min += Field_width;
         field_borders_reached = 1;
     }
-    else if (x_max >= field_width) {
-        x_max -= field_width;
+    else if (x_max >= Field_width) {
+        x_max -= Field_width;
         field_borders_reached = 1;
     }
     if (field_borders_reached == 1) {
@@ -233,11 +233,11 @@ long get_sector_with_offset(const PARTICLE* unit, long current_sector) {
     
     field_borders_reached = 0;
     if (y_min < 0) {
-        y_min += field_height;
+        y_min += Field_height;
         field_borders_reached = 1;
     }
-    else if (y_max >= field_height) {
-        y_max -= field_height;
+    else if (y_max >= Field_height) {
+        y_max -= Field_height;
         field_borders_reached = 1;
     }
     if (field_borders_reached == 1) {
@@ -262,9 +262,9 @@ long get_sector_with_offset(const PARTICLE* unit, long current_sector) {
  * @return -1 if there is not enough memory available for the 'realloc()'
  */
 int depot_add(const PARTICLE* unit) {
-    depot_a_length += 1;
-    depot_a = realloc(depot_a, depot_a_length * sizeof(PARTICLE));
-    depot_a[depot_a_length - 1] = *unit;
+    Depot_a_length += 1;
+    Depot_a = realloc(Depot_a, Depot_a_length * sizeof(PARTICLE));
+    Depot_a[Depot_a_length - 1] = *unit;
     return 0;
 }
 
@@ -275,12 +275,12 @@ int depot_add(const PARTICLE* unit) {
  * @noexcept
  */
 void depot_remove(int depot_a_id) {
-    if ((depot_a_id < 0) || (depot_a_id >= depot_a_length)) {
+    if ((depot_a_id < 0) || (depot_a_id >= Depot_a_length)) {
         return;
     }
-    depot_a[depot_a_id] = depot_a[depot_a_length - 1];
-    depot_a_length -= 1;
-    depot_a = realloc(depot_a, depot_a_length * sizeof(PARTICLE));
+    Depot_a[depot_a_id] = Depot_a[Depot_a_length - 1];
+    Depot_a_length -= 1;
+    Depot_a = realloc(Depot_a, Depot_a_length * sizeof(PARTICLE));
     errno = 0;
 }
 
@@ -293,12 +293,12 @@ void depot_remove(int depot_a_id) {
  * @return -1 if the 'realloc()' failed or a given ID is incorrect
  */
 int depot_die(int depot_a_id) {
-    if ((depot_a_id < 0) || (depot_a_id >= depot_a_length)) {
+    if ((depot_a_id < 0) || (depot_a_id >= Depot_a_length)) {
         return -1;
     }
-    depot_d_length += 1;
-    depot_d = realloc(depot_d, depot_d_length * sizeof(PARTICLE));
-    depot_d[depot_d_length - 1] = depot_a[depot_a_id];
+    Depot_d_length += 1;
+    Depot_d = realloc(Depot_d, Depot_d_length * sizeof(PARTICLE));
+    Depot_d[Depot_d_length - 1] = Depot_a[depot_a_id];
     depot_remove(depot_a_id);
     return 0;
 }
@@ -316,7 +316,7 @@ void* thread_processor(void* context) {
             return NULL;
         }
         
-        pthread_mutex_lock(&thread_processor_lock);
+        pthread_mutex_lock(&Thread_processor_lock);
     }
     
     struct msgbuf_particle particle_buffer;
@@ -329,7 +329,7 @@ void* thread_processor(void* context) {
             int stop_flag = 0;
             
             // Receive all messages in the queue
-            while (msgrcv(qid_receive,
+            while (msgrcv(Qid_receive,
                           &particle_buffer, SIZEOF_MSGBUF_PARTICLE,
                           0, IPC_NOWAIT) > 0) {
                 if (particle_buffer.type == CLASSIFIER_STOP) {
@@ -348,26 +348,26 @@ void* thread_processor(void* context) {
             }
         }
         
-        if (depot_a_length == 0) {
+        if (Depot_a_length == 0) {
             // Perform an immediate particle receive
             tick = -1;
             continue;
         }
         
-        for (int i = 0; i < depot_a_length; i++) {
-            if (depot_a[i].steps_to_live == 0) {
-                if (get_sector(&depot_a[i]) == mpi_rank) {
-                    particle_buffer.particle = depot_a[i];
+        for (int i = 0; i < Depot_a_length; i++) {
+            if (Depot_a[i].steps_to_live == 0) {
+                if (get_sector(&Depot_a[i]) == Mpi_rank) {
+                    particle_buffer.particle = Depot_a[i];
                     particle_buffer.type = CLASSIFIER_REPORT;
-                    msgsnd(qid_send,
+                    msgsnd(Qid_send,
                            &particle_buffer, SIZEOF_MSGBUF_PARTICLE, 0);
                     depot_die(i);
                     i -= 1;
                 }
                 else {
-                    particle_buffer.particle = depot_a[i];
+                    particle_buffer.particle = Depot_a[i];
                     particle_buffer.type = CLASSIFIER_NORM;
-                    msgsnd(qid_send,
+                    msgsnd(Qid_send,
                            &particle_buffer, SIZEOF_MSGBUF_PARTICLE, 0);
                     depot_remove(i);
                     i -= 1;
@@ -382,40 +382,40 @@ void* thread_processor(void* context) {
                 
                 while ((d_rand = randgen_get()) < -DOUBLE_ERROR) {}
                 
-                p_cursor += p_left;
+                p_cursor += P_left;
                 if (d_rand - p_cursor < DOUBLE_ERROR) {
-                    depot_a[i].x = (depot_a[i].x == 0) ?
-                                   (field_width - 1) :
-                                   (depot_a[i].x - 1);
+                    Depot_a[i].x = (Depot_a[i].x == 0) ?
+                                   (Field_width - 1) :
+                                   (Depot_a[i].x - 1);
                     d_rand = 10.0;
                 }
                 
-                p_cursor += p_up;
+                p_cursor += P_up;
                 if (d_rand - p_cursor < DOUBLE_ERROR) {
-                    depot_a[i].y = (depot_a[i].y == 0) ?
-                                   (field_height - 1) :
-                                   (depot_a[i].y - 1);
+                    Depot_a[i].y = (Depot_a[i].y == 0) ?
+                                   (Field_height - 1) :
+                                   (Depot_a[i].y - 1);
                     d_rand = 10.0;
                 }
                 
-                p_cursor += p_right;
+                p_cursor += P_right;
                 if (d_rand - p_cursor < DOUBLE_ERROR) {
-                    depot_a[i].x = (depot_a[i].x + 1) % field_width;
+                    Depot_a[i].x = (Depot_a[i].x + 1) % Field_width;
                     d_rand = 10.0;
                 }
                 
-                p_cursor += p_down;
+                p_cursor += P_down;
                 if (d_rand - p_cursor < DOUBLE_ERROR) {
-                    depot_a[i].y = (depot_a[i].y + 1) % field_height;
+                    Depot_a[i].y = (Depot_a[i].y + 1) % Field_height;
                     // d_rand = 10.0;
                 }
                 
-                depot_a[i].steps_to_live -= 1;
+                Depot_a[i].steps_to_live -= 1;
                 
-                if (get_sector_with_offset(&depot_a[i], mpi_rank) != mpi_rank) {
-                    particle_buffer.particle = depot_a[i];
+                if (get_sector_with_offset(&Depot_a[i], Mpi_rank) != Mpi_rank) {
+                    particle_buffer.particle = Depot_a[i];
                     particle_buffer.type = CLASSIFIER_NORM;
-                    msgsnd(qid_send,
+                    msgsnd(Qid_send,
                            &particle_buffer, SIZEOF_MSGBUF_PARTICLE, 0);
                     depot_remove(i);
                     i -= 1;
@@ -424,12 +424,12 @@ void* thread_processor(void* context) {
         }
     }
     
-    // Check 'depot_a'
-    if (depot_a_length != 0) {
+    // Check 'Depot_a'
+    if (Depot_a_length != 0) {
         fprintf(stderr, "A processing error occurred\n");
     }
     
-    pthread_mutex_unlock(&thread_processor_lock);
+    pthread_mutex_unlock(&Thread_processor_lock);
     
     sleep(5);
     
@@ -449,8 +449,8 @@ int main(int argc, char** argv) {
             return -1;
         }
         
-        MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-        MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &Mpi_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &Mpi_rank);
     }
     
     
@@ -463,29 +463,29 @@ int main(int argc, char** argv) {
         }
         
         // Convert arguments
-        l = strtol(argv[1], NULL, 10);
-        a = strtol(argv[2], NULL, 10);
-        b = strtol(argv[3], NULL, 10);
-        n = strtol(argv[4], NULL, 10);
-        N = strtol(argv[5], NULL, 10);
+        Base_l = strtol(argv[1], NULL, 10);
+        Base_a = strtol(argv[2], NULL, 10);
+        Base_b = strtol(argv[3], NULL, 10);
+        Base_n = strtol(argv[4], NULL, 10);
+        Base_N = strtol(argv[5], NULL, 10);
         
         N_dead = 0;
-        p_left = strtod(argv[6], NULL);
-        p_right = strtod(argv[7], NULL);
-        p_up = strtod(argv[8], NULL);
-        p_down = strtod(argv[9], NULL);
+        P_left = strtod(argv[6], NULL);
+        P_right = strtod(argv[7], NULL);
+        P_up = strtod(argv[8], NULL);
+        P_down = strtod(argv[9], NULL);
         
-        if (a * b != mpi_size) {
+        if (Base_a * Base_b != Mpi_size) {
             fprintf(stderr, "A number of MPI units is incorrect\n");
             return -1;
         }
         
-        offset = ((l / OFFSET_DIVISOR) > OFFSET_MAX) ?
+        Offset = ((Base_l / OFFSET_DIVISOR) > OFFSET_MAX) ?
                  (OFFSET_MAX) :
-                 (l / OFFSET_DIVISOR);
+                 (Base_l / OFFSET_DIVISOR);
         
-        field_width = a * l;
-        field_height = b * l;
+        Field_width = Base_a * Base_l;
+        Field_height = Base_b * Base_l;
     }
     
     struct timeval t_start;
@@ -510,26 +510,26 @@ int main(int argc, char** argv) {
     
     // Prepare and launch sender and receiver threads //
     {
-        if ((qid_receive = msgget(IPC_PRIVATE, 0666)) < 0) {
+        if ((Qid_receive = msgget(IPC_PRIVATE, 0666)) < 0) {
             fprintf(stderr, "Receive message queue cannot be created\n");
             return -1;
         }
-        if ((qid_send = msgget(IPC_PRIVATE, 0666)) < 0) {
+        if ((Qid_send = msgget(IPC_PRIVATE, 0666)) < 0) {
             fprintf(stderr, "Send message queue cannot be created\n");
             return -1;
         }
         
         if (randgen_init(QUEUE_RAND_NORMAL_LENGTH,
-                         (unsigned int)mpi_rank) < 0) {
+                         (unsigned int)Mpi_rank) < 0) {
             fprintf(stderr, "Random number generator cannot be launched\n");
             return -1;
         }
         
         // Prevent 'thread_processor' from doing anything
-        pthread_mutex_init(&thread_processor_lock, NULL);
-        pthread_mutex_lock(&thread_processor_lock);
+        pthread_mutex_init(&Thread_processor_lock, NULL);
+        pthread_mutex_lock(&Thread_processor_lock);
         
-        if (pthread_create(&thread_processor_tid, NULL,
+        if (pthread_create(&Thread_processor_tid, NULL,
                            thread_processor, NULL) < 0) {
             fprintf(stderr, "Processor thread cannot be launched\n");
             return -1;
@@ -541,29 +541,29 @@ int main(int argc, char** argv) {
     {
         srand48((long)time(NULL));
         
-        depot_a_length = (int)N;
-        if ((depot_a = malloc(depot_a_length * sizeof(PARTICLE))) == NULL) {
+        Depot_a_length = (int)Base_N;
+        if ((Depot_a = malloc(Depot_a_length * sizeof(PARTICLE))) == NULL) {
             fprintf(stderr, "malloc() / realloc() error\n");
             return -1;
         }
         
-        for (int i = 0; i < N; i++) {
-            depot_a[i].origin_sector = mpi_rank;
-            depot_a[i].steps_to_live = n;
-            depot_a[i].x = lrand48() % l;
-            depot_a[i].x += (mpi_rank % a) * l;
-            depot_a[i].y = lrand48() % l;
-            depot_a[i].y += (mpi_rank / a) * l;
+        for (int i = 0; i < Base_N; i++) {
+            Depot_a[i].origin_sector = Mpi_rank;
+            Depot_a[i].steps_to_live = Base_n;
+            Depot_a[i].x = lrand48() % Base_l;
+            Depot_a[i].x += (Mpi_rank % Base_a) * Base_l;
+            Depot_a[i].y = lrand48() % Base_l;
+            Depot_a[i].y += (Mpi_rank / Base_a) * Base_l;
         }
         
-        depot_d_length = 0;
-        depot_d = NULL;
+        Depot_d_length = 0;
+        Depot_d = NULL;
     }
     
     
     // Run main cycle of the program. 'main()' works with MPI //
     {
-        pthread_mutex_unlock(&thread_processor_lock);
+        pthread_mutex_unlock(&Thread_processor_lock);
         
         PARTICLE mpi_recv_holder;
         struct msgbuf_particle buffer_recv;
@@ -590,16 +590,16 @@ int main(int argc, char** argv) {
                     if (status.MPI_TAG == CLASSIFIER_REPORT) {
                         // Some particle has died
                         N_dead += 1;
-                        if (N_dead == N * mpi_size) {
+                        if (N_dead == Base_N * Mpi_size) {
                             // Cease operations: Send message to 'thread_processor'
                             buffer_recv.type = CLASSIFIER_STOP;
-                            msgsnd(qid_receive, &buffer_recv, SIZEOF_MSGBUF_PARTICLE, 0);
+                            msgsnd(Qid_receive, &buffer_recv, SIZEOF_MSGBUF_PARTICLE, 0);
                             break;
                         }
                     }
                     else {
                         buffer_recv.type = CLASSIFIER_NORM;
-                        msgsnd(qid_receive, &buffer_recv, SIZEOF_MSGBUF_PARTICLE, 0);
+                        msgsnd(Qid_receive, &buffer_recv, SIZEOF_MSGBUF_PARTICLE, 0);
                     }
                     MPI_Irecv(&mpi_recv_holder,
                               1, PARTICLE_mpi_t, MPI_ANY_SOURCE, MPI_ANY_TAG,
@@ -612,7 +612,7 @@ int main(int argc, char** argv) {
             {
                 MPI_Request dummy;
                 
-                if (msgrcv(qid_send,
+                if (msgrcv(Qid_send,
                            &buffer_send, SIZEOF_MSGBUF_PARTICLE, 0,
                            MSG_NOERROR | IPC_NOWAIT) < 0) {
                     errno = 0;
@@ -621,7 +621,7 @@ int main(int argc, char** argv) {
                 
                 if (buffer_send.type == CLASSIFIER_REPORT) {
                     // Send report to everyone (including self)
-                    for (int i = 0; i < mpi_size; i++) {
+                    for (int i = 0; i < Mpi_size; i++) {
                         MPI_Isend(&(buffer_send.particle), 1, PARTICLE_mpi_t,
                                   i,
                                   CLASSIFIER_REPORT, MPI_COMM_WORLD, &dummy);
@@ -650,16 +650,16 @@ int main(int argc, char** argv) {
     // Completion actions //
     {
         MPI_Type_free(&PARTICLE_mpi_t);
-        pthread_join(thread_processor_tid, NULL);
+        pthread_join(Thread_processor_tid, NULL);
         randgen_stop();
-        msgctl(qid_send, IPC_RMID, NULL);
-        msgctl(qid_receive, IPC_RMID, NULL);
+        msgctl(Qid_send, IPC_RMID, NULL);
+        msgctl(Qid_receive, IPC_RMID, NULL);
         errno = 0;
         
-        if (mpi_rank == MPI_MASTER_RANK) {
+        if (Mpi_rank == MPI_MASTER_RANK) {
             // Write statistics into file
             double elapsed_time_total = elapsed_time;
-            for (int i = 1; i < mpi_size; i++) {
+            for (int i = 1; i < Mpi_size; i++) {
                 MPI_Recv(&elapsed_time,
                          1, MPI_DOUBLE,
                          MPI_ANY_SOURCE, CLASSIFIER_TIME, MPI_COMM_WORLD, NULL);
@@ -699,13 +699,13 @@ int main(int argc, char** argv) {
     
     // Output //
     {
-        unsigned int particle_absolute_distribution[mpi_size];
-        for (int i = 0; i < mpi_size; i++) {
+        unsigned int particle_absolute_distribution[Mpi_size];
+        for (int i = 0; i < Mpi_size; i++) {
             particle_absolute_distribution[i] = 0;
         }
         
-        for (int i = 0; i < depot_d_length; i++) {
-            particle_absolute_distribution[depot_d[i].origin_sector] += 1;
+        for (int i = 0; i < Depot_d_length; i++) {
+            particle_absolute_distribution[Depot_d[i].origin_sector] += 1;
         }
         
         MPI_File datafile;
@@ -719,7 +719,7 @@ int main(int argc, char** argv) {
         
         MPI_File_write_ordered(datafile,
                                particle_absolute_distribution,
-                               mpi_size, MPI_UNSIGNED,
+                               Mpi_size, MPI_UNSIGNED,
                                MPI_STATUS_IGNORE);
         
         MPI_Barrier(MPI_COMM_WORLD);
@@ -727,8 +727,8 @@ int main(int argc, char** argv) {
     }
     
     
-    free(depot_a);
-    free(depot_d);
+    free(Depot_a);
+    free(Depot_d);
     
     MPI_Finalize();
     return 0;
